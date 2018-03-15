59824c8e4c969cdf32f406ef67960dd483e5cf7f
cnoeldev <cnoeldev@gmail.com>
2018-03-14 17:14:43 -0700

    Initial commit
    First commit, essential files only.

---
+++ YearCounter/YearCounter/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace YearCounter
+{
+    //A class for generation of people consisting of a first name, last name, and date of birth/death between 1900-2000.
+    class Person
+    {
+        string _name;
+        int[] _lifespan;
+
+        //getters for name and lifespan
+        public string Name
+        {
+            get { return _name; }
+        }
+        public int Birth {
+            get { return _lifespan[0]; }
+        }
+        public int Death {
+            get { return _lifespan[1]; }
+        }
+
+        public Person()
+        {
+            _name = Generator.MakeName();
+            _lifespan = Generator.MakeLifespan();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", Name, Birth, Death);
+        }
+    }
+
+    static class Generator
+    {
+        //Lists of names obtained from www2.census.gov
+        static string[] firstNames = File.ReadAllLines(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\Resources\FirstNames.txt");
+        static string[] lastNames = File.ReadAllLines(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\Resources\LastNames.txt");
+
+        static int minYear = 1900;
+        static int maxYear = 2000;
+
+        public static string MakeName()
+        {
+            Random random = new Random();
+            //select random first name from file
+            int randomLineNumber = random.Next(0, firstNames.Length);
+            string first = firstNames[randomLineNumber].Trim();
+            //select random last name from file
+            randomLineNumber = random.Next(0, lastNames.Length);
+            string last = lastNames[randomLineNumber].Trim();
+
+            //build name sting
+            string name = first + ',' + last;
+
+            return name;
+        }
+
+        public static int[] MakeLifespan()
+        {
+            //create random birth years from 1900 to 2000
+            Random random = new Random();
+            //adding 1 because random is exclusive ending, want to possibly get some with year 2000.
+            int birth = random.Next(minYear, maxYear + 1);
+            int death = random.Next(birth, maxYear + 1);
+
+            //build array representing lifespan
+            int[] lifespan = { birth, death };
+
+            return lifespan;
+        }
+    }
+}
---
+++ YearCounter/YearCounter/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace YearCounter
+{
+    class Program
+    {
+        static string _defaultPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\Resources\People.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Computing Result");
+
+            //This call was used to build the names and lifespans for use in selecting the most populated year(s)
+            BuildInitialFile(1000);
+
+            int result = GetMostLivingYear(_defaultPath);
+
+            Console.WriteLine(String.Format("The latest year with the most living people, exluding possible ties, is {0}", result));
+
+            Console.Write("\nFinished. Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static int GetMostLivingYear(string dataPath)
+        {
+            //living year stores the counts of people living in each year, 0-100 will represent 1900 to 2000
+            int[] livingYear = new int[101];
+            //using offset value to convert from 1900-2000 down to 0-100;
+            int offset = 1900;
+            //highest year store the position in livingYear currently holding the most living people.
+                //The case where multiple years have the same amount is considered trivial in this scenario.
+            int highestVal = 0;
+            int highestYear = 0;
+
+            //pull all lines from file, readalllines because readonly purpose
+            var lines = File.ReadAllLines(dataPath);
+            foreach(string l in lines)
+            {
+                //using split to separate names from dates.  Vaguely simulating handling a formatted but natural dataset.
+                string[] words = l.Split(' ');
+                //no-try parsing words, as we know from the file where the years are and that there can be no invalid numbers.
+                int start = int.Parse(words[1]) - offset;
+                int end = int.Parse(words[2]) - offset;
+                //iterating living year based on lifespan, to add a living count to each year properly.
+                for(int i = start; i <= end; i++)
+                {
+                    livingYear[i]++;
+                    //track the year with the most living people
+                    if(livingYear[i] > highestVal)
+                    {
+                        highestVal = livingYear[i];
+                        highestYear = i;
+                    }
+                }
+            }
+
+            //Add the offset back into the highest year so the result will be an actual year.
+            highestYear += offset;
+
+            foreach(int count in livingYear)
+            {
+                Console.WriteLine(String.Format("{0} living: {1}", offset, count));
+                //won't need offset anymore, so reuse it to increment for printing.
+                offset++;
+            }
+
+            return highestYear;
+        }
+
+        //Use the Person class to populate People.txt with a set of people and lifespans.  Size determines the number of people/lines.
+        private static void BuildInitialFile(int size)
+        {
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(_defaultPath);
+                for (int i = 0; i < size; i++)
+                {
+                    Person p = new Person();
+                    string line = p.ToString();
+                    streamWriter.WriteLine(line);
+                }
+                streamWriter.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Initial File Exception: " + e.Message);
+            }
+        }
+    }
+}
